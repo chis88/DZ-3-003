c2f27b9ca7b7f35a6b0b30feaad31655249ffd63
Ivan <chivvasregal94@gmail.com>
2023-02-14 14:17:36 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите одно число");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+ else
+ {
+    Console.WriteLine("Нет");
+ }
